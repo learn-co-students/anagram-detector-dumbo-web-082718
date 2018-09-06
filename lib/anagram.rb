0f09e87c05c12f3e177d2c87587283188960902d
jordanmmartin <jdordan23@gmail.com>
2018-09-06 17:32:24 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.select do |backwards_word|
+      backwards_word.split("").sort == word.split("").sort
+    end
+  end
+
+
+end
